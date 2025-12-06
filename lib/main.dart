0e07e0619a60342700cb4308ad0c5653fd5977b7
Chris <forklift2flutter@gmail.com>
2025-12-06 15:02:51 -0500

    changed color of button and more readable text style

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
+
       child: MaterialApp(
         title: 'Band Name Generator',
+
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF0047AA)),
         ),
         home: MyHomePage(),
       ),
@@ -36,11 +38,12 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [Text('How\'s this sound?'), 
-        Text(appState.current.asPascalCase),
+        BigCard(pair: pair),
       ElevatedButton(
             onPressed: () {
               appState.getNext();
@@ -49,6 +52,31 @@ class MyHomePage extends StatelessWidget {
 
           ),
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+ @override
+  Widget build(BuildContext context) {
+     final theme = Theme.of(context); 
+      final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary, 
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase, style: style,),
       ),
     );
   }
