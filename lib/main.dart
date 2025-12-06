372f2065cbbc5b256e1bb39bdbd5108fc00ae76b
Chris <forklift2flutter@gmail.com>
2025-12-06 15:31:57 -0500

    final before adding navigation rail

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,16 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -40,19 +50,45 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+     IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+    
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, 
           children: [Text('How\'s this sound?'), 
           BigCard(pair: pair),
-        ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
-        
-            ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min, 
+            
+            
+            
+            children: [
+              
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+              
+              ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                      
+                ),
+            ],
+          ),
           ],
         ),
       ),
@@ -79,7 +115,7 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary, 
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase, style: style,),
+        child: Text(pair.asPascalCase, style: style,),
       ),
     );
   }
